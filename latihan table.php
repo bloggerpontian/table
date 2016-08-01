cc35c923d2b64a12ee6c6156081e3e655fa4379a
bloggerpontian <bloggerpontian@gmail.com>
2016-08-01 16:26:21 +0800

    Add files via upload

---
+++ latihan table.php
@@ -0,0 +1,25 @@
+<strong>Malaysia States: </strong>
+<table border=1 cellpadding=0 cellspacing=0>
+   <tr>
+   <td>State Name</td>
+   </tr>
+   <tr>
+   <td>Ibu Negeri</td>
+   </tr>
+   <tr>
+   <td>Short Name</td>
+   </tr>
+   <tr>
+   <td>
+   <?php
+   $states = array("Selangor", "Johor", "Negeri Sembilan", "Melaka", "Perak", "Perlis", "Kelantan", "Terengganu", "Pulau Pinang", "Pahang", "Sabah", "Sarawak", "Kuala Lumpur", "Labuan", "Putrajaya");
+   foreach ($states as $state) {
+       echo $state;
+   }
+   ?>
+   </td></tr>
+   
+
+
+   
+</table>
---
+++ table.php
@@ -0,0 +1,9 @@
+<table>
+<tr>
+<td>
+<? php echo $bakery[0];?>
+</td></tr>
+<tr><td>
+<?php echo $baker[1];?>
+</td></tr>
+</table>
